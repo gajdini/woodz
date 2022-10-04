338acf366623d58851766128430d6bf30a4f6849
Greisa Ajdini <greisa.ajdini@bytebuzzer.com>
2022-10-04 04:44:49 +0200

    barcode scanner

--- app/src/main/java/com/tooz/woodz/BarcodeScannerActivity.kt
+++ app/src/main/java/com/tooz/woodz/BarcodeScannerActivity.kt
@@ -1,6 +1,112 @@
 package com.tooz.woodz
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.Detector.Detections
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import java.io.IOException
 
 class BarcodeScannerActivity : AppCompatActivity() {
+    var surfaceView: SurfaceView? = null
+    var txtBarcodeValue: TextView? = null
+    private lateinit var barcodeDetector: BarcodeDetector
+    private lateinit var cameraSource: CameraSource
+    private val REQUEST_CAMERA_PERMISSION = 201
+    var intentData = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_barcode)
+        initViews()
+    }
+
+    private fun initViews() {
+        txtBarcodeValue = findViewById(R.id.txtBarcodeValue)
+        surfaceView = findViewById(R.id.surfaceView)
+    }
+
+    private fun initialiseDetectorsAndSources() {
+//        Toast.makeText(applicationContext, "Barcode scanner started", Toast.LENGTH_SHORT).show()
+        barcodeDetector = BarcodeDetector.Builder(this)
+            .setBarcodeFormats(Barcode.ALL_FORMATS)
+            .build()
+        cameraSource = CameraSource.Builder(this, barcodeDetector)
+            .setRequestedPreviewSize(1920, 1080)
+            .setAutoFocusEnabled(true) //you should add this feature
+            .build()
+        surfaceView!!.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(
+                            this@BarcodeScannerActivity,
+                            Manifest.permission.CAMERA
+                        ) === PackageManager.PERMISSION_GRANTED
+                    ) {
+                        cameraSource.start(surfaceView!!.holder)
+                    } else {
+                        ActivityCompat.requestPermissions(
+                            this@BarcodeScannerActivity,
+                            arrayOf<String>(Manifest.permission.CAMERA),
+                            REQUEST_CAMERA_PERMISSION
+                        )
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                cameraSource.stop()
+            }
+        })
+        barcodeDetector.setProcessor(object : Detector.Processor<Barcode> {
+            override fun release() {
+               //todo
+            }
+
+            override fun receiveDetections(detections: Detections<Barcode>) {
+                //todo
+                //for now it shows the scanned value in the textview
+                //todo remove textview
+                val barcodes = detections.detectedItems
+                if (barcodes.size() != 0) {
+                    txtBarcodeValue!!.post {
+                            intentData = barcodes.valueAt(0).displayValue
+                            txtBarcodeValue!!.text = intentData
+                    }
+                }
+            }
+        })
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        cameraSource.release()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initialiseDetectorsAndSources()
+    }
+
+
 }
